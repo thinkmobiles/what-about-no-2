7b476c9943e8b0cd582b5ae8742db61aa9e1df3b
nazarovits <istvan.nazarovits@gmail.com>
2015-07-06 10:46:45 +0200

    8081 for production

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ PostGre.Collections = new Collections(PostGre);
 
 require('./routes/index')(app, PostGre);
 
-port = parseInt(process.env.PORT) || 8836;
+port = parseInt(process.env.PORT) || 8081;
 server = http.createServer(app);
 
 server.listen(port, function () {
